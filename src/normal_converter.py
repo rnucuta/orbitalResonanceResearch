7dcc1e59b1f3a3eb54ce07b43b24e0f5e2fc85c7
Raymond Nucuta <raymondnucuta@Raymonds-MacBook-Pro.local>
2020-02-28 10:26:47 -0700

    thermal map update

--- src/normal_converter.py
+++ src/normal_converter.py
@@ -5,7 +5,7 @@ import os
 dir_name=os.getcwd()
 dir_name=dir_name[0:len(dir_name)-3]+"models/"
 
-asteroid_stl = mesh.Mesh.from_file(dir_name+'toutatis.stl')
+asteroid_stl = mesh.Mesh.from_file(dir_name+'Steins.stl')
 
 normal_vectors=asteroid_stl.normals
 
--- src/thermal_map.py
+++ src/thermal_map.py
@@ -1,13 +1,24 @@
 from rays import Rays
 from facets import Facets
+import numpy as np
 
 class ThermalMap:
   #thermal snapshot per time step
-  def __init__(self):
+  def __init__(self, timestep):
     self.rays_obj=Rays()
-    self.facets_objs=[]
-  def initiate_facets_objects(self):
-    for
+    self.copy_vectors=self.rays_obj.np_asteroid_stl.vectors
+    self.normals=None
+    self.timestep=timestep
   def eliminate_bad_facets(self):
-
-  def line_intersections(self):
+  	distance=self.rays_obj.np_asteroid_stl.get_mass_properties()[1]
+  	indices_to_remove=[]
+  	for i in range(len(self.rays_obj.np_asteroid_stl.normals)):
+  		if np.dot(distance, self.rays_obj.np_asteroid_stl.normals[i])<0:
+  			indices_to_remove.append(i)
+  	return indices_to_remove
+  def shadowing(self):
+  	pass
+  def facets_temps():
+  	pass
+  def SignedVolume(a,b,c,d):
+	return (1.0/6.0)*np.dot(np.cross(np.subtract(b,a),np.subtract(c,a)),np.subtract(d,a))
