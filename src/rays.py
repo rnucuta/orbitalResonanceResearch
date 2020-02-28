bc8e31ec199cc03ba0ed9c635fb3e2397f4dc758
Raymond Nucuta <raymondnucuta@Raymonds-MacBook-Pro.local>
2020-02-28 10:47:13 -0700

    updated rays class with generate centroids method

--- src/rays.py
+++ src/rays.py
@@ -1,23 +1,24 @@
-from sympy import symbols
-x, y, z = symbols('x y z')
-import numpy as np
-import trimesh
-mesh=trimesh.load('Steins.stl')
-center_of_mass=mesh.center_mass
-import math
-from random import gauss
+# from sympy import symbols
+# x, y, z = symbols('x y z')
+# import numpy as np
+# import trimesh
+# mesh=trimesh.load('Steins.stl')
+# center_of_mass=mesh.center_mass
+# import math
+# from random import gauss
 import numpy
 from stl import mesh
 
 class Rays:
   def __init__(self):
-    self.ast_com=center_of_mass
-    self.ast_mesh=mesh
+    # self.ast_com=center_of_mass
+    # self.ast_mesh=mesh
     #need to update the number of triangles
-    self.number_of_rays=1.5*20480
     self.rays_array=[]
     self.np_asteroid_stl = mesh.Mesh.from_file('Steins.stl')
-  def ray_emitter(self, location_of_sun, location_of_asteroid):
+    self.number_of_rays=len(np_asteroid_stl.vectors)
+    self.centroids=self.generate_centroids()
+  # def ray_emitter(self, location_of_sun, location_of_asteroid):
     #plane of asteroid is an array of length 4: [a,b,c,d]
     #ax+by+cz=d
     #ast_radius=((3/4)*self.ast_mesh.bounding_sphere.volume*(1/math.pi))**(1/3)
@@ -32,6 +33,15 @@ class Rays:
     #   temp[i]=temp[i]*ast_radius
     # unit_vector.append(temp)
     # return unit_vector
+  def generate_centroids(self):
+    temp=[0,0,0]
+    for i in range(self.number_of_rays):
+        for j in range(3):
+            temp[0]+=np_asteroid_stl.vectors[i][j][0]/3
+            temp[1]+=np_asteroid_stl.vectors[i][j][1]/3
+            temp[2]+=np_asteroid_stl.vectors[i][j][2]/3
+        self.centroids.append(temp)
+        temp=[0,0,0]
   def generate_all_rays(self, location_of_asteroid):
     for i in range(self.number_of_rays):
       self.rays_array.append(self.ray_emitter([0,0,0], location_of_asteroid))
--- src/thermal_map.py
+++ src/thermal_map.py
@@ -4,11 +4,11 @@ import numpy as np
 
 class ThermalMap:
   #thermal snapshot per time step
-  def __init__(self, timestep):
+  def __init__(self, starting_timestep):
     self.rays_obj=Rays()
     self.copy_vectors=self.rays_obj.np_asteroid_stl.vectors
     self.normals=None
-    self.timestep=timestep
+    self.timestep=starting_timestep
   def eliminate_bad_facets(self):
   	distance=self.rays_obj.np_asteroid_stl.get_mass_properties()[1]
   	indices_to_remove=[]
@@ -17,8 +17,11 @@ class ThermalMap:
   			indices_to_remove.append(i)
   	return indices_to_remove
   def shadowing(self):
-  	pass
-  def facets_temps():
+  	indices_to_remove=self.eliminate_bad_facets()
+  	for i in range(len(self.rays_obj.np_asteroid_stl.normals)):
+  		if i not in indices_to_remove:
+
+  def facets_temps(self):
   	pass
   def SignedVolume(a,b,c,d):
 	return (1.0/6.0)*np.dot(np.cross(np.subtract(b,a),np.subtract(c,a)),np.subtract(d,a))
