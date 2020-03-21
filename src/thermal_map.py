14cf4610fdbbe30e2de550353dd74937f598e117
Raymond <rnucuta@gmail.com>
2020-03-20 20:28:38 -0700

    raymoo's and este's shit

--- src/thermal_map.py
+++ src/thermal_map.py
@@ -10,20 +10,38 @@ class ThermalMap:
     self.normals=None
     self.timestep=starting_timestep
   def eliminate_bad_facets(self):
-  	distance=self.rays_obj.np_asteroid_stl.get_mass_properties()[1]
-  	indices_to_remove=[]
-  	for i in range(self.rays_obj.number_of_rays):
-  		if np.dot(distance, self.rays_obj.np_asteroid_stl.normals[i])<0:
-  			indices_to_remove.append(i)
-  	return indices_to_remove
+    distance=self.rays_obj.np_asteroid_stl.get_mass_properties()[1]
+    indices_to_remove=[]
+    for i in range(self.rays_obj.number_of_rays):
+      if np.dot(distance, self.rays_obj.np_asteroid_stl.normals[i])<0:
+        indices_to_remove.append(i)
+    return indices_to_remove
+  
+  #shadowing code
   def shadowing(self):
-  	indices_to_remove=self.eliminate_bad_facets()
-  	for i in range(self.rays_obj.number_of_rays):
-  		if i not in indices_to_remove:
-
-  def facets_temps(self):
-  	pass
+    indices_to_remove=self.eliminate_bad_facets()
+    rays_generated=self.rays_obj.generate_all_rays(self.rays_obj.np_asteroid_stl.get_mass_properties()[1])
+    non_shadowed_array_indices=[]
+    q1=self.rays_obj.np_asteroid_stl.get_mass_properties()[1]*2
+    q2=self.rays_obj.np_asteroid_stl.get_mass_properties()[1]*0.05
+    for i in range(self.rays_obj.number_of_rays):
+      ray_temp=[]
+      for j in range(self.rays_obj.number_of_rays):
+        if j not in indices_to_remove and j not in non_shadowed_array_indices:
+          p1=self.rays_obj.np_asteroid_stl.vectors[j][0]
+          p2=self.rays_obj.np_asteroid_stl.vectors[j][0]
+          p3=self.rays_obj.np_asteroid_stl.vectors[j][0]
+          if SignedVolume(q1,p1,p2,p3)*SignedVolume(q1,p1,p2,p3)<0 and np.sign(SignedVolume(q1,q2,p1,p2))==np.sign(SignedVolume(q1,q2,p2,p3))==np.sign(SignedVolume(q1,q2,p3,p1)):
+            ray_temp.append(j)
+      distances=[]
+      for ray in ray_temp:
+        distances.append(np.linalg.norm(self.rays_obj.centroids[ray]))
+      non_shadowed_array_indices.append(ray_temp[distances.index(min(distances))])
+    return non_shadowed_array_indices
+#p1,p2,p3 triangle; q1,q2 points on the line far away in both direction
+    #If SignedVolume(q1,p1,p2,p3) and SignedVolume(q2,p1,p2,p3) have different signs AND SignedVolume(q1,q2,p1,p2), SignedVolume(q1,q2,p2,p3) and SignedVolume(q1,q2,p3,p1) have the same sign, then there is an intersection
   def SignedVolume(self,a,b,c,d):
-  	#p1,p2,p3 triangle; q1,q2 points on the line far away in both direction
-  	#If SignedVolume(q1,p1,p2,p3) and SignedVolume(q2,p1,p2,p3) have different signs AND SignedVolume(q1,q2,p1,p2), SignedVolume(q1,q2,p2,p3) and SignedVolume(q1,q2,p3,p1) have the same sign, then there is an intersection
-	return (1.0/6.0)*np.dot(np.cross(np.subtract(b,a),np.subtract(c,a)),np.subtract(d,a))
+    return (1.0/6.0)*np.dot(np.cross(np.subtract(b,a),np.subtract(c,a)),np.subtract(d,a))
+  
+  def facets_temps(self):
+    pass
--- src/yarkovsky.py
+++ src/yarkovsky.py
@@ -1,36 +1,50 @@
-from thermal import thermal
+from rays import np_asteroid_stl
+import numpy as np
+import temp
+
 class Yarkovsky:
-  def crossProd(a,b):
-      dimension = len(a)
-      c = []
-      for i in range(dimension):
-        c.append(0)
-        for j in range(dimension):
-          if j <> i:
-            for k in range(dimension):
-              if k <> i:
-                if k > j:
-                  c[i] += a[j]*b[k]
-                elif k < j:
-                  c[i] -= a[j]*b[k]
-      return c
-  k=-(2*5.67 * 10^-8)/(3*2.998*10^8)
-   #def __init__(self):
-     #ang_v=something
-     #sont need, see astroid class?
-    #i honestly don't know math behind this shit/initial stuff to keep in mind
-  def force_vectors(self):
-    f= k*eccentricity*sum(thermal^4*normal_vectors)
-    #f=c*integrate(emissitivity*T^4*normal)
-    #return force vectors on asteroid
-    return f
-  def torque_vectors(self):
-    tor=k*eccentricity*sum(thermal^4*crossProd((facet_centroid_center_of_mass),normal_vectors))
-    #take cross product of radius from com to facet centroid
-    #return torque vectors on asteroid
-    return tor
-  def spin(self):
-    #return YORP spin effects on asteroid
-    #do initial condition plus timestep effects and then set them as new initial (how tf do u do that)
-    #IF YOU SEE THIS COMMENT, TELL US THAT UR DOING WORK
-    #im working homie damn, i got home saturday evening
+  def __init__(self):
+  def yarkovskyforce(self)
+  emissivity = 0.73
+  boltzmann = 1.38064852 * 10^(-23)
+  speedoflight = 2.998 * 10^8
+  forcelist = []
+  temporaryforce = None
+  for t in range(len(thermalmap())):
+    temporaryforce = []
+    for f in range(len(np_asteroid_stl.vectors)):
+      facetforce = np_asteroid_stl.normals(f)*(thermalmap(t)(f))^(4) * -2 * boltzmann * emissivity / (3*speedoflight)
+      temporaryforce = np.add(temporaryforce,facetforce)
+    forcelist.append(temporaryforce)
+  finalforce = [0,0,0]
+  for x in forcelist:
+    finalforce = np.add(finalforce,x)
+  return np.divide(finalforce,len(thermalmap()))
+
+
+  def yorptorque(self)
+  torquelist = []
+  temporarytorque = None
+  for t in range(len(thermalmap())):
+    temporarytorque = []
+    for f in range(len(np_asteroid_stl.vectors)):
+      centroid = np.divide(np.add(np.add(np_asteroid_stl.v0,np_asteroid_stl.v1),np_asteroid_stl.v2),3)
+      facetforce = np_asteroid_stl.normals(f)*(thermalmap(t)(f))^(4) * -2 * boltzmann * emmissivity / (3*speedoflight)
+      facettorque = np.cross(facetforce,centroid)
+      temporarytorque = np.add(temporarytorque,facettorque)
+    torquelist.append(temporarytorque)
+  finaltorque = [0,0,0]
+  for x in torquelist:
+    finaltorque = np.add(finaltorque,x)
+  return np.divide(finaltorque,len(thermalmap()))
+
+
+  def phi(sunvec):
+    facetlist = []
+    for f in range(len(np_asteroid_stl.vectors)):
+      philist = []
+      for t in range(len(thermalmap())):
+        philist.append(np.divide(np.dot(sunvec,np_asteroid_stl.normals(f))),(np.linalg.norm(sunvec)*np.linalg.norm(np_asteroid_stl.normals(f))))
+        np_asteroid_stl.rotate(axis)
+      facetlist.append(philist)
+    return facetlist
