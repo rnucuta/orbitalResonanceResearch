4cda1e57efce6dcabac5befe7adf26fbe5c99d37
Raymond <rnucuta@gmail.com>
2020-03-27 11:02:54 -0700

    fixed file path in main.py

--- src/main.py
+++ src/main.py
@@ -1,6 +1,6 @@
 #IMPORT yarkovsky.py
 
 def save_yark_vector(np_vec):
-	with open('final_yark_vector', 'w') as f:
+	with open('final_yark_vector.txt', 'w') as f:
     	for item in list(np_vec):
         	f.write("%s\n" % item)
