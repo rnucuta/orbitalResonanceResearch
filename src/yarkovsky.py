4d7bb5d931c9e2bad04be3b8cac5854b82a821a9
Raymond <rnucuta@gmail.com>
2020-03-30 21:39:06 -0700

    temp object in yarkovsky

--- src/yarkovsky.py
+++ src/yarkovsky.py
@@ -15,7 +15,7 @@ class Yarkovsky:
     speedoflight = 2.998 * 10^8
     forcelist = []
     temporaryforce = None
-    therm_map= Temperature.temp()
+    therm_map= self.Temperature.temp()
     for t in range(len(therm_map)):
       temporaryforce = []
       for f in range(len(self.np_asteroid_stl.vectors[1])):
