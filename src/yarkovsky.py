36d2d5c6f748be1d1513a2365f3ef4e04484ace4
Raymond <rnucuta@gmail.com>
2020-04-08 00:21:39 -0700

    temp obj save fixed

--- src/yarkovsky.py
+++ src/yarkovsky.py
@@ -12,10 +12,10 @@ class Yarkovsky:
     self.Temperature=None
     if not self.load_temp:
       self.Temperature=Temperature(0.01, 400, 50)
-      with open('./temp_obj.data', 'wb') as f:
+      with open('./temp_obj.obj', 'w') as f:
         pickle.dump(self.Temperature, f)
     else:
-      with open('./temp_obj.data', 'rb') as f:
+      with open('./temp_obj.obj', 'r') as f:
         self.Temperature=pickle.load(f)
     self.np_asteroid_stl=self.Temperature.thermalmap_obj.rays_obj.np_asteroid_stl
     self.therm_map= self.Temperature.temp()
