e32df23d18d5f97a78f5519443e657cdc18aa032
Mowteam <Whrosenberg9@gmail.com>
2020-04-14 11:01:16 -0700

    v

--- src/thermal_map.py
+++ src/thermal_map.py
@@ -12,7 +12,6 @@ class ThermalMap:
     self.position = self.rays_obj.position
     self.copy_vectors=self.rays_obj.np_asteroid_stl.vectors
     self.normals=None
-    self.v=[-0.0059690746,-0.0163998975,-0.9998476952]
   def eliminate_bad_facets(self):
     distance = self.position
     indices_to_remove=[]
@@ -55,14 +54,14 @@ class ThermalMap:
     pass
 
   def orient(self):
-    v = self.v
+    v = [-0.0059690746,-0.0163998975,-0.9998476952]
     itheta = math.acos(0.9998476952)
     axis = np.cross(v,[0,0,-1])
     self.rays_obj.np_asteroid_stl.rotate(axis,itheta)
     self.rays_obj.np_asteroid_stl.update_normals()
 
   def rotation(self,timesteps):
-    v = self.v
+    v = [0.0059690746, 0.0163998975, 0.9998476952]
     self.rays_obj.np_asteroid_stl.rotate(v,math.radians(360/timesteps))
     self.rays_obj.np_asteroid_stl.update_normals()
 
