910e338ae10ebf8b18d2fb16fe31f2c021ef81b3
Mowteam <Whrosenberg9@gmail.com>
2020-04-16 12:05:30 -0700

    display changed

--- src/display.py
+++ src/display.py
@@ -17,68 +17,45 @@ import mpl_toolkits.mplot3d as a3
 import numpy as np
 import scipy as sp
 from scipy import spatial as sp_spatial
+import pickle
 # from temp import te
-from stl_editor import angles, shadows
+#from stl_editor import angles, shadows
 
 def main():
-	# angle = angles(400)
-	shadow = shadows(400)
-	#print(shadow)
-	final_temps = 0#te()
+
+	#shadow
+	with open('./shadow_data.data', 'rb') as f:
+		shadow = pickle.load(f)
+
+	#thermal map
+	with open('./temp_obj.obj', 'rb') as f:
+		Temperature = pickle.load(f)
+	final_temps = Temperature.final_temps
+
+	#color
 	hexes = []
 	time = 0
+	mini = min(final_temps[time])
+	maxi = max(final_temps[time])
 	for i in range(206):
-		#T = final_temps[time][i]
-		#mini = min(final_temps[time])
-		#maxi = max(final_temps[time])
-		#value = RGB(T, mini, maxi)
-		hexes.append('#FF0000')
-		#hexes.append('#FF0000')
+		T = final_temps[time][i]
+		value = RGB(T, mini, maxi)
+		hexes.append(value)
 
 	
+
+
+	#plotting
 	fig = plt.figure()
 	ax = fig.add_subplot(111, projection='3d')
 	ax.set_xlim3d(-100, 100)
 	ax.set_ylim3d(-100, 100)
 	ax.set_zlim3d(-100, 100)
 	file_name = '207tri_sphere.stl'
-	#sphere_mesh1 = mesh.Mesh.from_file(file_name)
 	plotter = pv.Plotter()
 	sphere_mesh = pv.read(file_name)
 
-	# fc = []
-	# for i in range(206):
-	# 	if i % 2 == 0:
-	# 		fc.append('white')
-	# 	else:
-	# 		fc.append('orange')
-
-	# fc = hexes 
-
-	# color = []
-
-	# for i in fc:
-	# 	color.append(i.upper())
-
 	
-	# print(color)
-
-
-
-
-
-
-
-
-
-	#plotter.add_mesh(sphere_mesh, scalars = color)
-	#plotter.show()
-
-
-
-
-
-
 	x = []
 	y = []
 	z = []
@@ -89,7 +66,7 @@ def main():
 	faces = []
 	values = sphere_mesh.faces
 	for i in range(206):
-		if shadow[i][0] == 1:
+		if i in shadow[0] or True:
 			face = []
 			index = 1
 			for j in range(3):
@@ -106,54 +83,12 @@ def main():
 			y.append(vertices[i[j]][1])
 			z.append(vertices[i[j]][2])
 		verts = [list(zip(x, y, z))]
-		ax.add_collection3d(Poly3DCollection(verts))
+		ax.add_collection3d(Poly3DCollection(verts, hexes))
 		x = []
 		y = []
 		z = []
 
 	plt.show()
-
-
-
-
-
-
-
-
-
-
-	# normal = sphere_mesh.face_normals
-	# normal1 = sphere_mesh1.normals
-
-	# #print(normal)
-
-
-	# for i in range(len(normal1)):
-	# 	mag = np.linalg.norm(normal1[i])
-	# 	normal1[i][0] = normal1[i][0]/mag
-	# 	normal1[i][1] = normal1[i][1]/mag
-	# 	normal1[i][2] = normal1[i][2]/mag
-	
-	#print(normal1)
-
-
-	# test = False
-	# for i in range(sphere_mesh.number_of_faces):
-	# 	for j in range(3):
-	# 		if not sphere_mesh.face_normals[i][j] == sphere_mesh1.normals[i][j]:
-	# 			test = True
-
-	# if test:
-	# 	print("FUCK")
-	# else:
-	# 	print("YAY")
-	
-
-
-
-
-
-
 
 def RGB(T, mini, maxi):
 	Rt = R(T, mini, maxi)
