f0db951cf1d98c2cddc18afd024a2f0d9e291ca1
Raymond <rnucuta@gmail.com>
2020-12-04 11:47:12 -0700

    thermal_cython.c created

--- src/setup.py
+++ src/setup.py
@@ -3,10 +3,17 @@
 import distutils.core
 import Cython.Build
 import argparse
+import numpy
 
-parser = argparse.ArgumentParser()
-parser.add_argument("-f","--filename", help="python file name to cythonize", type=str)
-args=parser.parse_args()
+
+# parser = argparse.ArgumentParser()
+# parser.add_argument("-f","--filename", help="python file name to cythonize", type=str)
+# args=parser.parse_args()
+
+# distutils.core.setup(
+#     ext_modules = Cython.Build.cythonize(args.filename))
 
 distutils.core.setup(
-    ext_modules = Cython.Build.cythonize(args.filename))
+    ext_modules = Cython.Build.cythonize("thermal_cython.pyx"),
+    include_dirs=[numpy.get_include()]
+    )
